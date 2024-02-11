0f36c806a26cb81e95843485c10c80d386b19a90
Ayush SIngh Bhadoria <76483606+marooncoder09@users.noreply.github.com>
2024-02-11 12:50:55 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Footer from "../components/Footer";
+import SectionHero from "./SectionHero";
+import SectionLetsConnect from "./SectionLetsConnect";
+import SectionMyLatestProject from "./SectionMyLatestProject";
+import SectionQuote from "./SectionQuote";
+import SectionTechnologyStack from "./SectionTechnologyStack";
+
+export default function Home() {
+  return (
+    <div className="safe-layout">
+      <SectionHero />
+      <SectionQuote />
+      <SectionTechnologyStack />
+      
+      <SectionMyLatestProject />
+      <SectionLetsConnect />
+
+      <Footer />
+    </div>
+  )
+}
---
+++ constant/assets.ts
@@ -0,0 +1,80 @@
+export const assets = {
+  home: {
+    hero: {
+      avatarSmile: require("@images/home/hero/Untitleddesign1.png"),
+      dribble: require("@images/home/hero/dribble.webp"),
+      github: require("@images/home/hero/github.webp"),
+      instagram: require("@images/home/hero/instagram.webp"),
+      linkedin: require("@images/home/hero/linkedin.webp"),
+      quadrilateral: require("@images/home/hero/quadrilateral.webp"),
+      triangle: require("@images/home/hero/triangle.webp"),
+      twistedTorus: require("@images/home/hero/twisted-torus.webp"),
+      zigZag: require("@images/home/hero/zig-zag.webp"),
+    },
+    letsConnect: {
+      avatarBigSmile: require("@images/home/hero/Untitleddesign1.png"),
+      dribble: require("@images/home/letsConnect/dribble.webp"),
+      github: require("@images/home/letsConnect/github.webp"),
+      gmail: require("@images/home/letsConnect/gmail.webp"),
+      instagram: require("@images/home/letsConnect/instagram.webp"),
+      linkedin: require("@images/home/letsConnect/linkedin.webp"),
+    },
+    myLatestProject: {
+      figma: require("@images/home/myLatestProject/figma.webp"),
+      rocket: require("@images/home/myLatestProject/rocket.webp"),
+      suitcase: require("@images/home/myLatestProject/suitcase.webp"),
+      projects: {
+        portfolioWebsite: require("@images/home/myLatestProject/projects/portfolio-website.webp"),
+      },
+    },
+    quote: {
+      bulb: require("@images/home/quote/bulb.webp"),
+      heart: require("@images/home/quote/heart.webp"),
+      paintKit: require("@images/home/quote/paint-kit.webp"),
+      teaCup: require("@images/home/quote/tea-cup.webp"),
+    },
+    technologyStack: {
+      expo: require("@images/home/technologyStack/expo.webp"),
+      expressJs: require("@images/home/technologyStack/express-js.webp"),
+      javascript: require("@images/home/technologyStack/javascript.webp"),
+      laravel: require("@images/home/technologyStack/laravel.webp"),
+      nextJs: require("@images/home/technologyStack/next-js.webp"),
+      planetScale: require("@images/home/technologyStack/planet-scale.webp"),
+      prisma: require("@images/home/technologyStack/prisma.webp"),
+      reactJs: require("@images/home/technologyStack/react-js.webp"),
+      reactNative: require("@images/home/technologyStack/react-native.webp"),
+      tailwindCss: require("@images/home/technologyStack/tailwindcss.webp"),
+      typescript: require("@images/home/technologyStack/typescript.webp"),
+    },
+    events: {
+      openingCeremony: require("@images/home/events/day1/openingCeremony.webp"),
+      scienceExh: require("@images/home/events/day1/scienceExh.webp"),
+      cinema: require("@images/home/events/day1/cinema.webp"),
+      fashionShow: require("@images/home/events/day1/fashionShow.webp"),
+      paintain: require("@images/home/events/day1/paintain.webp"),
+      codingChallenge: require("@images/home/events/day1/codingChallenge.webp"),
+      cos: require("@images/home/events/day1/cos.webp"),
+      ecoKart: require("@images/home/events/day1/ecoKart.webp"),
+      bridgeMaking: require("@images/home/events/day1/bridgeMaking.webp"),
+      workShop: require("@images/home/events/day1/workShop.webp"),
+
+      designC: require("@images/home/events/day1/designC.webp"),
+      eSportsComp: require("@images/home/events/day1/eSportsComp.webp"),
+      captureFlag: require("@images/home/events/day1/captureFlag.webp"),
+      hunting: require("@images/home/events/day1/hunting.webp"),
+      mona: require("@images/home/events/day1/mona.webp"),
+      jamming: require("@images/home/events/day1/jamming.webp"),
+      dancing: require("@images/home/events/day1/dancing.webp"),
+      comedy: require("@images/home/events/day1/comedy.webp"),
+      
+      closingCeremony: require("@images/home/events/day1/closingCeremony.webp"),
+      awards: require("@images/home/events/day1/awards.webp"),
+      star: require("@images/home/events/day1/star.webp"),
+      bandWar: require("@images/home/events/day1/bandWar.webp"),
+
+
+
+      
+  },
+}
+};
